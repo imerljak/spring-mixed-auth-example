0e2562e500b3087760a02b200a3eb6eab433c8b9
Israel Merljak <imerljak@gmail.com>
2020-08-05 20:27:42 -0300

    init git

---
+++ src/main/java/com/example/mixedauth/MixedAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.mixedauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixedAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixedAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mixedauth/MixedAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mixedauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixedAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
